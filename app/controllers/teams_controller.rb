090ec8a09637764eead93d4cf54eac05e7b09ae9
Carlos Gochez <cgotcha@gmail.com>
2013-10-19 11:34:12 -0600

    acciones de new, edit, index

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -7,14 +7,30 @@ class TeamsController < ApplicationController
   end
 
   def new
+    @team = Team.new
   end
 
   def create
+    @team = Team.new(params[:team])
+
+    if @team.save
+      redirect_to teams_path
+    else
+      render :new
+    end
   end
 
   def edit
+    @team = Team.find(params[:id])
   end
 
   def update
+    @team = Team.find(params[:id])
+
+    if @team.update_attributes(params[:team])
+      redirect_to teams_path
+    else
+      render :edit
+    end
   end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,5 @@
 class Team < ActiveRecord::Base
   attr_accessible :address, :country, :first_div, :founded_at, :name, :phone
+  validates_presence_of :name, :country
+  validates_uniqueness_of :phone, allow_blank: true
 end
